2b5d4ce0279d95522eeee2a622ea35f37b93f575
Eric QU <69889630+Daozheng-QU@users.noreply.github.com>
2022-08-15 14:58:11 +0100

    Update large nodes.py

--- large nodes.py
+++ large nodes.py
@@ -4,17 +4,7 @@
 #MSc of Data Science and AI
 #COMP702 Project of Modularity in empire network
 #Supervisor:Michele Zito,Keith Dures.
-'''Acknowledgement:
 
-I am very grateful to my first supervisor for answering too many questions about the project,
-such as understanding errors during the conversion of the Empire graph, such as improving code efficiency, etc.,
-I also thanks to Senior Chen Sheng for giving me inspiration on the traversal (single line of code) on the opposite side of the empire graph,
-which will enable me to better complete the transformation of the empire graph.'''
-'''Ethical issues and data protection:
-
-This project does not use any data of others, does not involve any ethical issues, and only uses publicly available resources for reference.
-It is strictly forbidden to use the project data and materials without the consent of the project supervisor and person in charge.
-'''
 import networkx as nx
 import numpy as np
 import pandas as pd
